7f39891e1c6d701d95b391a1ecaeb0df192e2766
mutifix <139007103+mutifix@users.noreply.github.com>
2024-02-18 23:37:27 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+    BotToken: "",//Bot Token
+    ServerID: "",// Server Id
+    Prefix: ["."],// botun öneki
+    BotOwners: [""],// bot owners
+    MongoURL: "", //mongo url
+    BotVoiceChannel: "", //Botun Bağlacağı Ses Kanal IDsi
+    BotStatus: "muti was here", // Bot Durum
+
+    MemberRole: "",//member role
+    SecretRoomCategory: "",//secret category
+    ChatChannel: "",//chat channel
+    WelcomeChannel: "",//Welcome Channel
+
+
+    // bunlar ship icin önemli erkek kız rolü filan varsa atayin
+    ManRoles: "",//erkek rol 
+    WomanRoles: "",//kız rol 
+}
+
+
+//welcome mesajındaki arka plan için picture klasörüne yükelyip bağlantı verebilirsiniz
+
+
+//discord: mutifix / ertu
+
+
+// hata alırsanız ulaşın
